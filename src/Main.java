87f8361871943feef775aacd0f9ccf5bd12b5bf2
max <kabinidi@mail.ru>
2022-12-06 18:29:12 +0300

    Ok 2

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,26 @@
 public class Main {
 
 
+    public static void PrintM(int[] m) {
 
-    public static void PrintM ( int [] m){
+        for (int i = 0; i < m.length; i++) {
+            if (i > 2 && i < 8) {
+                if (m[i] % 2 != 0) {
+                    m[i] = m[i] + 1;
+                }
+                System.out.println(m[i]);
+            }
+        }
 
-        for (int i =0 ; i < m.length ;i++) {
-            if (m[i] % 2 != 0) {
-                m[i] = m[i] + 1;
-            }System.out.println(m[i]);
+
+        }
+
+
+        public static void main (String[]args){
+            PrintM(new int[]{1, 2, 3, 4, 5, 6, 7, 8});
+
+
         }
 
 
     }
-
-
-
-    public static void main(String[] args) {
-        PrintM( new int[] {1,2,3,4,5,6,7,8});
-
-
-
-
-}
-
-
-
-
-}
